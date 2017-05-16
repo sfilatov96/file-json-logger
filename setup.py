bfbb0c4c90b472a7f29282fa30898cffa0851b76
sfilatov96 <stepa-filatov@yandex.ru>
2017-05-16 17:49:08 +0300

    repair setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,8 @@
 
 from setuptools import setup, find_packages
 
+with open('README.md', 'r') as f:
+    readme = f.read()
 
 setup(
     name='file-json-logger',
@@ -10,12 +12,13 @@ setup(
     packages=find_packages(),
     requires=['python (>= 2.5)'],
     description='It allows you save json-logs into file. Special for Kibana',
+    long_description=readme,
     author='Stepan Filatov',
     author_email='stepa-filatov@yandex.ru',
     url='https://github.com/sfilatov96/file-json-logger',
     download_url='https://github.com/sfilatov96/file-json-logger/tarball/master',
     license='MIT License',
-    keywords='json-logger',
+    keywords=['json-logger', "file-json-logger", ""],
     classifiers=[
         'Intended Audience :: Developers',
         'Programming Language :: Python',
